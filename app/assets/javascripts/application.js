2a0927db4d3aff30d0f20009a825f35a01e47b13
Frank Zhu <frankyufengzhu@gmail.com>
2014-10-07 21:52:32 -0400

    Tried different table settings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require dataTables
+//= require dataTables.jqueryui
+//= require jquery.dataTables.min
 //= require dataTables.foundation
 //= require pickadate/picker
 //= require pickadate/picker.date
